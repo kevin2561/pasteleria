25ecae6e14d33d45018a68c121675b0ee683dc9a
kevin <kevinnoa02@gmail.com>
2024-09-30 20:14:36 -0500

    parte 8

--- js/index.js
+++ js/index.js
@@ -1,22 +1,15 @@
 function main() {
     let paginas = ["Tienda", "Nosotros", "Blog", "Contacto"]
+    let cuenta=document.getElementById("cuenta")
     let menuPrincipal = document.querySelectorAll("#menu-principal li .pagina");
     let mainContext = document.getElementById("main-context");
+
     menuPrincipal.forEach((a, index) => {
-        a.addEventListener("click", function (e) {
-            e.preventDefault();
-            let pagina = paginas[index];
-            fetch(`./paginas/${paginas[index]}.html`)
-                .then(response => response.text())
-                .then(data => {
-                    mainContext.innerHTML = data;
-                    // window.history.pushState({ page: pagina }, pagina, `/${pagina.toLowerCase()}`);
-
-                })
-
-
-        })
+        let pagina = paginas[index];
+        a.addEventListener("click", (e) => vinculos(pagina, e))
     })
+
+
     window.addEventListener("scroll", function () {
         let headerNav = document.querySelector(".header-nav");
         let header = document.querySelector("header");
@@ -34,6 +27,21 @@ function main() {
 
 
     })
+    cuenta.addEventListener("click",(e) => vinculos("Cuenta", e))
+
+
+    function vinculos(pagina, e) {
+        e.preventDefault();
+        fetch(`./paginas/${pagina}.html`)
+            .then(response => response.text())
+            .then(data => {
+                mainContext.innerHTML = data;
+                // window.history.pushState({ page: pagina }, pagina, `/${pagina.toLowerCase()}`);
+
+            })
+
+
+    }
 
 
 }
