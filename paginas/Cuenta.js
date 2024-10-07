0b87554f764eeeabb2006c025a54994b036761d8
KEVIN NOA ROMERO <kevinnoa02@gmail.com>
2024-10-07 12:00:45 -0500

    12 kevin

--- paginas/Cuenta.js
+++ paginas/Cuenta.js
@@ -1,5 +1,27 @@
-function cuenta(){
+function cuenta() {
+    let iconoMostrar = "bi bi-eye-slash-fill"
+    let iconoOcultar = "bi bi-eye-fill"
 
-    console.log("CUENTA JS")
+    let iconoMostrarContraseña = document.querySelectorAll(".mostrar-password");
+    let inputPassword = document.querySelectorAll(".password");
+    let iconos = document.querySelectorAll("i");
+
+    iconoMostrarContraseña.forEach((item, index) => {
+        item.addEventListener("click", (e) => {
+           // let icon = iconos[index].className
+            console.log(iconos[index].className)
+            if (inputPassword[index].type === "password") {
+                inputPassword[index].type = "text";
+
+                iconos[index].className = iconoMostrar
+            } else {
+                inputPassword[index].type = "password";
+                iconos[index].className = iconos[index].className
+            }
+
+
+        })
+    })
+
 }
 cuenta()
